713cf00264b5cc3f6db1481b222d7f2c69f0dca7
funashi88 <funahiro.com@gmail.com>
2020-11-28 10:37:06 +0900

    rubocopによるコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,27 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
-  VALID_NAME_KANA_REGEX = /\A[ァ-ン]+\z/
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  VALID_NAME_KANA_REGEX = /\A[ァ-ン]+\z/.freeze
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
 
-  with_options presence: true do       
+  with_options presence: true do
     validates :nickname
     validates :birthday
 
-    with_options format:{ with: VALID_NAME_REGEX, 
-                          message: "は全角（漢字・ひらがな・カタカナ）で入力してください。" } do
+    with_options format: { with: VALID_NAME_REGEX,
+                           message: 'は全角（漢字・ひらがな・カタカナ）で入力してください。' } do
       validates :last_name
       validates :first_name
     end
 
-    with_options format:{ with: VALID_NAME_KANA_REGEX, 
-                          message: "は全角（カタカナ）で入力してください。" } do
+    with_options format: { with: VALID_NAME_KANA_REGEX,
+                           message: 'は全角（カタカナ）で入力してください。' } do
       validates :katakana_last_name
       validates :katakana_first_name
     end
   end
 
-    validates :password, format: { with: VALID_PASSWORD_REGEX,
-                                   message: "is include both letters and numbers" }
+  validates :password, format: { with: VALID_PASSWORD_REGEX,
+                                 message: 'is include both letters and numbers' }
 end
